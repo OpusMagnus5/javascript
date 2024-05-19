0e60710647a0fdb78ddbdcd159c1f81fb7a8a55b
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-05-19 11:37:28 +0200

    variables

---
+++ JS/fundamentals/variables.js
@@ -0,0 +1,16 @@
+//Zmienne
+let message; //deklaracja zmiennej
+message = 'message'; //przypisanie wartości
+console.log(message);
+
+let messageInlineAssignment = 'messageInlineAssignment'; //deklaracja i przypisanie w jednej linii
+console.log(messageInlineAssignment);
+
+message = 'message changed'; //zmiana wartości zmiennej
+console.log(message);
+
+message = messageInlineAssignment;// przypisanie wartości zmiennej z innej zmiennej
+console.log(message)
+
+//Stałe
+const myBirthday = '01.05.1995'; //deklaracja zmiennej, nie można zmienić jej wartości
