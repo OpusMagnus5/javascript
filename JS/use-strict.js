32d25b300db60c1c86c10d5a2daded075649c82b
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-05-19 11:05:52 +0200

    use strict

---
+++ JS/use-strict.js
@@ -0,0 +1,3 @@
+"use strict"
+//Włącza nowe rzeczy wprowadzone w ECMAScript 5, ma to na celu domyślnie wspierać wcześniejsze wersje ES
+//Musimy więc włączyć, ale jeśli używamy nowych rzeczy z ES jak klasy czy moduły które zastosowane automatycznie stosują włączają use strict
